fc2f5e241e4c2782aff6ab2db2681c774d2eddab
Zoriot <zoriot@proton.me>
2026-04-19 22:27:53 +0200

    feat(utils): redesign MapLinks with config model, iteration, and provider toggles

--- utils/src/main/java/com/alpsbte/alpslib/utils/MapLink.java
+++ utils/src/main/java/com/alpsbte/alpslib/utils/MapLink.java
@@ -1,3 +1,9 @@
 package com.alpsbte.alpslib.utils;
 
+/**
+ * Resolved map provider link.
+ *
+ * @param name display name of the provider
+ * @param url absolute URL to open the location in that provider
+ */
 public record MapLink(String name, String url) {}
